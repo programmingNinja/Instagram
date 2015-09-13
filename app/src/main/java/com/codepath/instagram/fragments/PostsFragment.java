3ba821dd44d86317bde1ef856dd619158264ad0e
Nick Aiwazian <naiwazian@xiaoc-mbp.local.DHCP.thefacebook.com>
2015-09-12 23:56:15 -0700

    Code formatting

--- app/src/main/java/com/codepath/instagram/fragments/PostsFragment.java
+++ app/src/main/java/com/codepath/instagram/fragments/PostsFragment.java
@@ -32,167 +32,174 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PostsFragment extends Fragment {
-    private static final String TAG = "PostsFragment";
+  private static final String TAG = "PostsFragment";
 
-    private InstagramPostsAdapter instagramPostsAdapter;
-    InstagramClientDatabase database;
+  private InstagramPostsAdapter instagramPostsAdapter;
+  InstagramClientDatabase database;
 
-    private OnFragmentInteractionListener listener;
+  private OnFragmentInteractionListener listener;
 
-    RecyclerView rvInstagramPosts;
-    SwipeRefreshLayout swpContainer;
+  RecyclerView rvInstagramPosts;
+  SwipeRefreshLayout swpContainer;
 
-    public static PostsFragment newInstance() {
-        return new PostsFragment();
-    }
+  public static PostsFragment newInstance() {
+    return new PostsFragment();
+  }
 
-    public PostsFragment() {
-        // Required empty public constructor
-    }
+  public PostsFragment() {
+    // Required empty public constructor
+  }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        List<InstagramPost> posts = new ArrayList<>();
-        instagramPostsAdapter = new InstagramPostsAdapter(posts);
-        database = InstagramClientDatabase.getInstance(MainApplication.sharedApplication());
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+    List<InstagramPost> posts = new ArrayList<>();
+    instagramPostsAdapter = new InstagramPostsAdapter(posts);
+    database = InstagramClientDatabase.getInstance(MainApplication.sharedApplication());
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_posts, container, false);
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_posts, container, false);
 
-        rvInstagramPosts = (RecyclerView)view.findViewById(R.id.rvInstagramPosts);
-        swpContainer = (SwipeRefreshLayout)view.findViewById(R.id.swpContainer);
+    rvInstagramPosts = (RecyclerView) view.findViewById(R.id.rvInstagramPosts);
+    swpContainer = (SwipeRefreshLayout) view.findViewById(R.id.swpContainer);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),
-                LinearLayoutManager.VERTICAL, false);
-        layoutManager.scrollToPosition(0);
-        rvInstagramPosts.setLayoutManager(layoutManager);
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),
+            LinearLayoutManager.VERTICAL, false);
+    layoutManager.scrollToPosition(0);
+    rvInstagramPosts.setLayoutManager(layoutManager);
 
-        RecyclerView.ItemDecoration itemDecoration = new SimpleVerticalSpacerItemDecoration(10);
-        rvInstagramPosts.addItemDecoration(itemDecoration);
+    RecyclerView.ItemDecoration itemDecoration = new SimpleVerticalSpacerItemDecoration(10);
+    rvInstagramPosts.addItemDecoration(itemDecoration);
 
-        rvInstagramPosts.setAdapter(instagramPostsAdapter);
-        swpContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                fetchPosts();
-            }
-        });
-
-        swpContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-
+    rvInstagramPosts.setAdapter(instagramPostsAdapter);
+    swpContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override
+      public void onRefresh() {
         fetchPosts();
-        return view;
+      }
+    });
+
+    swpContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light);
+
+    fetchPosts();
+    return view;
+  }
+
+  private void fetchPosts() {
+    if (!isNetworkAvailable()) {
+      AlertDialogFragment.showAlertDialog(
+              getChildFragmentManager(),
+              getString(R.string.network_error),
+              getString(R.string.network_unavailable));
+      List<InstagramPost> posts = database.getAllInstagramPosts();
+      instagramPostsAdapter.replaceAll(posts);
+
+      return;
     }
 
-    private void fetchPosts() {
-        if (!isNetworkAvailable()) {
-            AlertDialogFragment.showAlertDialog(getChildFragmentManager(), getString(R.string.network_error),
-                    getString(R.string.network_unavailable));
-            List<InstagramPost> posts = database.getAllInstagramPosts();
-            instagramPostsAdapter.replaceAll(posts);
-
-            return;
-        }
-
-        if (listener != null) {
-            listener.showProgressBar();
-        }
-        startPostsFetcherService();
+    if (listener != null) {
+      listener.showProgressBar();
     }
+    startPostsFetcherService();
+  }
 
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+  private boolean isNetworkAvailable() {
+    ConnectivityManager connectivityManager
+            = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+    return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+  }
+
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+    try {
+      listener = (OnFragmentInteractionListener) activity;
+    } catch (ClassCastException e) {
+      throw new ClassCastException(activity.toString()
+              + " must implement OnFragmentInteractionListener");
     }
+  }
 
+  @Override
+  public void onResume() {
+    super.onResume();
+    // Register for the particular broadcast based on ACTION string
+    IntentFilter filter = new IntentFilter(PostsFetcherService.ACTION);
+    LocalBroadcastManager.getInstance(getActivity()).registerReceiver(postsFetchedReceiver, filter);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    // Unregister the listener when the application is paused
+    LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(postsFetchedReceiver);
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    listener = null;
+  }
+
+  public interface OnFragmentInteractionListener {
+    void showProgressBar();
+
+    void hideProgressBar();
+  }
+
+  @Override
+  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    inflater.inflate(R.menu.menu_home, menu);
+    super.onCreateOptionsMenu(menu, inflater);
+  }
+
+  private void handleNewPostsReceivedSuccess(List<InstagramPost> newPosts) {
+    if (listener != null) {
+      listener.hideProgressBar();
+    }
+    instagramPostsAdapter.replaceAll(newPosts);
+    swpContainer.setRefreshing(false);
+  }
+
+  private void handlerNewPostsReceivedFailure() {
+    if (listener != null) {
+      listener.hideProgressBar();
+    }
+    AlertDialogFragment.showAlertDialog(
+            getChildFragmentManager(),
+            getString(R.string.network_error),
+            getString(R.string.network_error));
+    swpContainer.setRefreshing(false);
+  }
+
+  private void startPostsFetcherService() {
+    Activity activity = getActivity();
+    if (activity != null) {
+      Intent intent = new Intent(activity, PostsFetcherService.class);
+      activity.startService(intent);
+    }
+  }
+
+  private BroadcastReceiver postsFetchedReceiver = new BroadcastReceiver() {
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            listener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onReceive(Context context, Intent intent) {
+      int resultCode = intent.getIntExtra(
+              PostsFetcherService.EXTRA_RESULT_CODE, Activity.RESULT_CANCELED);
+      if (resultCode == Activity.RESULT_OK) {
+        InstagramPosts postsWrapper = (InstagramPosts) intent.getSerializableExtra(
+                PostsFetcherService.EXTRA_RESULT_POSTS);
+        handleNewPostsReceivedSuccess(postsWrapper.posts);
+      } else {
+        handlerNewPostsReceivedFailure();
+      }
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Register for the particular broadcast based on ACTION string
-        IntentFilter filter = new IntentFilter(PostsFetcherService.ACTION);
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(postsFetchedReceiver, filter);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        // Unregister the listener when the application is paused
-        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(postsFetchedReceiver);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        listener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        void showProgressBar();
-        void hideProgressBar();
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_home, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    private void handleNewPostsReceivedSuccess(List<InstagramPost> newPosts) {
-        if (listener != null) {
-            listener.hideProgressBar();
-        }
-        instagramPostsAdapter.replaceAll(newPosts);
-        swpContainer.setRefreshing(false);
-    }
-
-    private void handlerNewPostsReceivedFailure() {
-        if (listener != null) {
-            listener.hideProgressBar();
-        }
-        AlertDialogFragment.showAlertDialog(getChildFragmentManager(), getString(R.string.network_error),
-                getString(R.string.network_error));
-        swpContainer.setRefreshing(false);
-    }
-
-    private void startPostsFetcherService() {
-        Activity activity = getActivity();
-        if (activity != null) {
-            Intent intent = new Intent(activity, PostsFetcherService.class);
-            activity.startService(intent);
-        }
-    }
-
-    private BroadcastReceiver postsFetchedReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            int resultCode = intent.getIntExtra(PostsFetcherService.EXTRA_RESULT_CODE, Activity.RESULT_CANCELED);
-            if (resultCode == Activity.RESULT_OK) {
-                InstagramPosts postsWrapper = (InstagramPosts)intent.getSerializableExtra(PostsFetcherService.EXTRA_RESULT_POSTS);
-                handleNewPostsReceivedSuccess(postsWrapper.posts);
-            } else {
-                handlerNewPostsReceivedFailure();
-            }
-        }
-    };
+  };
 }
--- app/src/main/java/com/codepath/instagram/fragments/SearchTagsResultFragment.java
+++ app/src/main/java/com/codepath/instagram/fragments/SearchTagsResultFragment.java
@@ -23,58 +23,61 @@ import org.json.JSONObject;
 import java.util.List;
 
 public class SearchTagsResultFragment extends Fragment {
-    private static final String TAG = "SearchTagResultsFragment";
+  private static final String TAG = "SearchTagResultsFragment";
 
-    RecyclerView rvSearchResults;
+  RecyclerView rvSearchResults;
 
-    protected SearchTagResultsAdapter searchResultsAdapter;
+  protected SearchTagResultsAdapter searchResultsAdapter;
 
-    public static SearchTagsResultFragment newInstance() {
-        return new SearchTagsResultFragment();
-    }
+  public static SearchTagsResultFragment newInstance() {
+    return new SearchTagsResultFragment();
+  }
 
-    public SearchTagsResultFragment() {
-        // Required empty public constructor
-    }
+  public SearchTagsResultFragment() {
+    // Required empty public constructor
+  }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        searchResultsAdapter = new SearchTagResultsAdapter();
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    searchResultsAdapter = new SearchTagResultsAdapter();
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search_tags_result, container, false);
-        rvSearchResults = (RecyclerView)view.findViewById(R.id.rvSearchResults);
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_search_tags_result, container, false);
+    rvSearchResults = (RecyclerView) view.findViewById(R.id.rvSearchResults);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),
-                LinearLayoutManager.VERTICAL, false);
-        rvSearchResults.setLayoutManager(layoutManager);
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),
+            LinearLayoutManager.VERTICAL, false);
+    rvSearchResults.setLayoutManager(layoutManager);
 
-        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(view.getContext(),
-                DividerItemDecoration.VERTICAL_LIST);
-        rvSearchResults.addItemDecoration(itemDecoration);
+    RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(view.getContext(),
+            DividerItemDecoration.VERTICAL_LIST);
+    rvSearchResults.addItemDecoration(itemDecoration);
 
-        rvSearchResults.setAdapter(searchResultsAdapter);
+    rvSearchResults.setAdapter(searchResultsAdapter);
 
-        return view;
-    }
+    return view;
+  }
 
-    public void fetchSearchResults(String searchTerm) {
-        MainApplication.getRestClient().getTagSearch(searchTerm, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                List<InstagramSearchTag> searchTags = Utils.decodeSearchTagsFromJsonResponse(response);
-                searchResultsAdapter.replaceAll(searchTags);
-            }
+  public void fetchSearchResults(String searchTerm) {
+    MainApplication.getRestClient().getTagSearch(searchTerm, new JsonHttpResponseHandler() {
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        List<InstagramSearchTag> searchTags = Utils.decodeSearchTagsFromJsonResponse(response);
+        searchResultsAdapter.replaceAll(searchTags);
+      }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                Log.wtf(TAG, "Network request failed");
-            }
-        });
-    }
-
+      @Override
+      public void onFailure(
+              int statusCode,
+              Header[] headers,
+              Throwable throwable,
+              JSONObject errorResponse) {
+        Log.wtf(TAG, "Network request failed");
+      }
+    });
+  }
 }
